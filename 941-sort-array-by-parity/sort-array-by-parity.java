534b1e1796d62cda66985c7acc4df727273e2e46
santoshipatel <santoshipatel811@gmail.com>
2026-07-14 00:36:14 +0530

    Time: 1 ms (51.11%) | Memory: 47 MB (31.04%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -1,24 +1,26 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int n = nums.length;
-        int j = 0;
-        int[] res = new int[n];
-       for(int i = 0 ; i<n; i++){
-        if(nums[i] % 2 == 0){
-            res[j] = nums[i];
-            j++;
-        }
-       }
-       for(int i = 0; i<n; i++){
-        if(nums[i] % 2 != 0){
-            res[j] = nums[i];
-            j++;
-        }
-       }
- return res;
-    }
-    }
-
-
+        int n = nums.length; // 1. 'n' ko define kiya
+        int[] arr = nums;    // 2. 'arr' ko nums se reference kiya taaki aapka logic distrub na ho
         
+        int[] copy = new int[n];
+        int a = 0;
+        int b = n - 1;
         
+        // Aapke loop ke jaise hi hum pooray array ko traverse karenge
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                copy[a++] = arr[i]; // Even numbers ko shuruat se daalenge
+            } else {
+                copy[b--] = arr[i];  // Odd numbers ko peeche se daalenge
+            }
+        }
+        
+        // Copy array se wapas main arr mein data transfer kiya aapke logic ki tarah
+        for (int i = 0; i < n; i++) {
+            arr[i] = copy[i];
+        }
+        
+        return arr;
+    }
+}
