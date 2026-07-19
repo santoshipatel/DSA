90dbc5543b940d62cbab2c1f948ffe4668619374
santoshipatel <santoshipatel811@gmail.com>
2026-07-19 16:53:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (29.34%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,17 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       int[][] ans = new int[m][n];
-       for(int j=0; j<n; j++){
-           ans[0][j] = 1;
+        int[][] ans = new int[m][n];
+        return totalPaths(0, 0, m, n, ans);
+    }
+    private int totalPaths(int i, int j, int m, int n, int[][] ans) {
+        if (i == m - 1 && j == n - 1) {
+            return 1;
         }
-       for(int i=0; i<m; i++){
-           ans[i][0] = 1;
+        if (i >= m || j >= n) {
+            return 0;
         }
-        for(int i=1; i<m; i++){
-          for(int j=1; j<n; j++){
-            ans[i][j] = ans[i][j-1] + ans[i-1][j];
-          }
+        if (ans[i][j] != 0) {
+            return ans[i][j];
         }
-        return ans[m-1][n-1];
+        int moveDown = totalPaths(i + 1, j, m, n, ans);
+        int moveRight = totalPaths(i, j + 1, m, n, ans);
+        ans[i][j] = moveDown + moveRight;
+        return ans[i][j];
     }
 }
